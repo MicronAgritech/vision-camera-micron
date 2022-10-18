c56da80fd8f175b2367d3e4440472ae8ae4bd52b
Ryan Williams <r.williams@micronagritech.com>
2022-10-18 20:29:08 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ interface ImageLabel {
  *
  * This algorithm executes within **~60ms**, so a frameRate of **16 FPS** perfectly allows the algorithm to run without dropping a frame. Anything higher might make video recording stutter, but works too.
  */
-export function labelImage(frame: Frame): ImageLabel[] {
+export function autoStart(frame: Frame): ImageLabel[] {
   'worklet';
   // @ts-expect-error Frame Processors are not typed.
-  return __labelImage(frame);
+  return __autoStart(frame);
 }
